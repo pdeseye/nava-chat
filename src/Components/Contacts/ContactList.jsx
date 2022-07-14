001bcd3f5666b9dcb7a191db5af69e098812b13f
Payal Desai <payal@idesai.com>
2022-07-14 16:45:05 -0400

    implement search feature

--- src/Components/Contacts/ContactList.jsx
+++ src/Components/Contacts/ContactList.jsx
@@ -1,10 +1,27 @@
-import React from "react"
+import {React, useState} from "react"
 import Contact from "./Contact"
 
 const ContactList = (props) => {
+  const [inputText, setInputText] = useState("")
+
+  let inputHandler = (e) => {
+    const search = e.target.value
+    setInputText(search)
+  }
+
+  const filteredContacts = props.contacts.filter(contact => {
+    if (inputText === '') {
+        return contact;
+    } else {
+        return contact.name.includes(inputText)
+    }
+})
+
   return (
     <>
-      {props.contacts.sort(function(a){
+      <input onChange={inputHandler}/>
+
+      {filteredContacts.sort(function(a){
         if(a.active){
           return -1
         } else {

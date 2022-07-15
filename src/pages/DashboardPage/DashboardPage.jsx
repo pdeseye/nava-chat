94c2e0b6a168a5bd015801de792725d7b729a126
Payal Desai <payal@idesai.com>
2022-07-15 13:03:09 -0400

    chat window displays contact with active chat

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,41 +1,99 @@
-import React from "react"
+import React, { useState } from "react"
 import ChatWindow from "../../Components/ChatWindow/ChatWindow"
 import ContactList from "../../Components/Contacts/ContactList"
 
 const DashboardPage = () => {
-
   const contacts = [
     {
       name: "Jane Doe",
       telephoneNumber: 4845992239,
-      active: true
+      active: true,
+      conversation: [
+      //   {sender: "user",
+      //   text: "heyyy",
+      //   time: "12:00",
+      // },
+      // {
+      //   sender:"Jane",
+      //   text: "hello",
+      //   time: "12:05"
+      // }
+    ]
+  },
+  {
+    name: "John Doe",
+    telephoneNumber: 4845992237,
+    active: false,
+    conversation: [
+      {sender: "user",
+      text: "heyyy",
+      time: "12:00",
     },
     {
-      name: "John Doe",
-      telephoneNumber: 4845992237,
-      active: false
-    },
-    {
-      name: "Georgia Miller",
-      telephoneNumber: 6100237465,
-      active: false
-    },
-    {
-      name: "Michael Jackson",
-      telephoneNumber: 6102513497,
-      active: false
-    },
-    {
-      name: "Barack Obama",
-      telephoneNumber: 1800237657,
-      active: true
-    },
+      sender:"John",
+      text: "hello",
+      time: "12:05"
+    }
   ]
+},
+// {
+//   name: "Georgia Miller",
+//   telephoneNumber: 6100237465,
+//   active: false,
+//   conversation: [
+//     {sender: "user",
+//     text: "heyyy",
+//     time: "12:00",
+//   },
+//   {
+//     sender:"Georgia",
+//     text: "hello",
+//     time: "12:05"
+//   }
+// ]
+// },
+// {
+//   name: "Michael Jackson",
+//   telephoneNumber: 6102513497,
+//   active: false,
+//   conversation: [
+//     {sender: "user",
+//     text: "heyyy",
+//     time: "12:00",
+//   },
+//   {
+//     sender:"Michael",
+//     text: "hello",
+//     time: "12:05"
+//   }
+// ]
+// },
+// {
+//   name: "Barack Obama",
+//   telephoneNumber: 1800237657,
+//   active: true,
+//   conversation: [
+//     {sender: "user",
+//     text: "heyyy",
+//     time: "12:00",
+//   },
+//   {
+//     sender:"Barack",
+//     text: "hello",
+//     time: "12:05"
+//   }
+// ]
+// },
+]
+const hasConversation = contacts.find(contact => contact.conversation.length > 0) ;
 
-  return(
-    <>
-      <ContactList contacts={contacts}/>
-      <ChatWindow/>
+const [contact, setContact] = useState(hasConversation ? hasConversation : null )
+
+
+return(
+  <>
+      <ContactList contacts={contacts} setContact={setContact}/>
+      <ChatWindow contact={contact}/>
     </>
   )
 }

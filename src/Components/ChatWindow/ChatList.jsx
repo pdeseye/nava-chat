f804050b64689da69b07cf94dc3c1684c2046080
Payal Desai <payal@idesai.com>
2022-07-20 13:31:50 -0400

    implement working form for chat

--- src/Components/ChatWindow/ChatList.jsx
+++ src/Components/ChatWindow/ChatList.jsx
@@ -1,9 +1,17 @@
+import React, { useEffect, useState } from "react"
 import Chat from "./Chat"
 
 const ChatList = (props) => {
+  const [chats, setChats] = useState([])
+  console.log(props.chats)
+  useEffect(() => {
+    setChats(props.chats)
+  }, [props.chats, chats])
+
+
   return(
     <div className="chatlist">
-    {props.chats.map((chat, idx) => 
+    {chats.map((chat, idx) => 
       <Chat key={idx} chat={chat}/>
     )}
     </div>
--- src/Components/ChatWindow/ChatWindow.jsx
+++ src/Components/ChatWindow/ChatWindow.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ChatList from "./ChatList";
 
 const ChatWindow = (props) => {
+  const [convo, setConvo] = useState([])
+  const [formData, setFormData] = useState({})
+
+  useEffect (() => {
+    setConvo(props.contact.conversation)
+  }, [props.contact])
+
+
+  const handleChange = (e) => {
+    const newConvo = {
+      sender: "user",
+      text: e.target.value,
+      time: "12:30",
+    }
+    setFormData( newConvo)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log( formData)
+    const tempconvo = [...convo, formData]
+    setConvo(tempconvo)
+    console.log(typeof tempconvo)
+  }
+
   return (
     <div className="chatwindow">
       <div> To: {props.contact.name}</div>
-      <ChatList chats={props.contact.conversation}/>
-      <form className="chatform">
-        <input type="text" />
+      <ChatList chats={convo}/>
+      <form className="chatform" onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange}/>
         <button type="submit">Send</button>
       </form>
     </div>
